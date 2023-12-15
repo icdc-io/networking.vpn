34fabcc79a08a6ba418fc6bad67c231a38b370d8
Pavel_Kazlou <pnkazlou@ibagroup.eu>
2023-12-15 17:08:16 +0300

    fix statistics url

--- src/components/clientConnectionDevices.js
+++ src/components/clientConnectionDevices.js
@@ -54,7 +54,7 @@ const ClientConnectionDevices = ({ t, history }) => {
 
     useEffect(() => {
         if (account && devicesIds) {
-            ws.current = new WebSocket(`ws${protocol}://${locationUrl.replace("api", "wireguard-wss")}/ws/wireguard_manager/stats?${devicesIds}`, ['actioncable-v1-json', window.insights.getToken(), account, role]);
+            ws.current = new WebSocket(`ws${protocol}://${locationUrl}/ws/wireguard_manager/stats?${devicesIds}`, ['actioncable-v1-json', window.insights.getToken(), account, role]);
             ws.current.onopen = () => {
                 const subscribe_msg = {
                     command: 'subscribe',
